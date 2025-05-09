5dca098e675ea13d418036129987818287da1fa7
Nihal Nayak <nihalnayak@gmail.com>
2025-05-09 09:44:29 -0400

    mmlu pro evaluatio

---
+++ lm_eval/tasks/cross_difficulty/utils.py
@@ -0,0 +1,34 @@
+from functools import partial
+
+
+choices = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+]
+
+
+def format_example(example, including_answer=True):
+    prompt = "Question:\n"
+    question = example["question"]
+    options = example["options"]
+    prompt += question + "\n"
+    prompt += "Options:\n"
+    for i, opt in enumerate(options):
+        prompt += "{}. {}\n".format(choices[i], opt)
+    return prompt
+
+mmlu_pro_doc_to_text = partial(format_example, including_answer=False)
